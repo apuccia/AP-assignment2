6beedbdd5b1670d7653f2634c2b864083d05020e
apuccia <alessandropuccia97@gmail.com>
2021-01-10 13:51:26 +0100

    implemented singlethread benchmark

---
+++ part3/benchmark.py
@@ -0,0 +1,98 @@
+from functools import wraps
+from statistics import mean, variance
+import threading
+import csv
+import time
+
+def benchmark(warmups=0, iter=1, verbose=False, csv_file=None):
+    def inner_function(func):
+        @wraps(func)
+        def wrapper_benchmark(*args, **kwargs):
+            if warmups:
+                times = []
+                print("\n--------------WARMUPS PHASE--------------")
+                print("Warmup\t\t| Time needed")
+                for i in range(warmups):
+                    start_time = time.perf_counter()
+                    func(*args, **kwargs)
+                    end_time = time.perf_counter()
+
+                    if verbose:
+                        res = end_time - start_time
+                        times.append([i + 1, True, res])
+                        print(f"{i + 1}\t\t| {res}")
+
+                if verbose:
+                    print("*****************************************")
+                    collapsed_times = [t[2] for t in times]
+                    print (f"Average time\t| {mean(collapsed_times)}")
+                    if len(times) > 1: print (f"Variance\t| {variance(collapsed_times)}")
+                    print("*****************************************")
+                    if (csv_file):
+                        to_csv(csv_file, times)
+
+            print("\n----------------ITER PHASE---------------")
+            print("Iter\t\t| Time needed")
+            average_time = 0
+            times = []
+            for i in range(iter):
+                start_time = time.perf_counter()
+                func(*args, **kwargs)
+                end_time = time.perf_counter()
+
+                res = end_time - start_time
+                times.append((i + 1, False, res))
+                average_time += res 
+                print(f"{i + 1 }\t\t| {res}")
+
+            print("*****************************************")
+            collapsed_times = [t[2] for t in times]
+            print (f"Average time\t| {mean(collapsed_times)}")
+            if len(times) > 1: print (f"Variance\t| {variance(collapsed_times)}")
+            print("*****************************************")
+
+            if (csv_file):
+                to_csv(csv_file, times)
+
+            
+        return wrapper_benchmark
+    return inner_function
+
+
+#@benchmark(iter=5, verbose=True, csv_file="prova.csv")
+def fibonacci(n=30):
+    def fibo(n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return fibo(n-1) + fibo(n-2)
+
+    fibo(n)
+
+
+def test(f):
+    def test_f(nthreads=1, ntimes=1):
+        @benchmark(iter=ntimes, verbose=True, csv_file=f"f_{nthreads}_{ntimes}.csv")
+        def wrapper(*args, **kwargs):
+            f(*args, **kwargs)
+
+        return wrapper
+
+    test_f(1, 16)()
+    test_f(2, 8)()
+    test_f(4, 4)()
+    test_f(8, 2)()
+
+
+def to_csv(csv_file, results):
+    with open(csv_file, 'a') as result_file:
+        writer = csv.writer(result_file, quoting=csv.QUOTE_MINIMAL)
+        for t in results:
+            writer.writerow(t)
+
+
+if __name__ == "__main__":
+    test(fibonacci)
+
