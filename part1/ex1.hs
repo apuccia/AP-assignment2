9c4a7c8540675a08e98aef7886ea40ddc76803e7
apuccia <alessandropuccia97@gmail.com>
2021-01-11 13:09:51 +0100

    added explanation for exercise 2 and general comments

--- part1/ex1.hs
+++ part1/ex1.hs
@@ -19,21 +19,28 @@ wf :: Eq a => ListBag a -> Bool
 wf (LB []) = True
 wf (LB ((x, y):xs)) = count x (toList (LB xs)) == 0
 
+-- constructors
+
 empty :: ListBag a
 empty = LB []
 
 singleton :: a -> ListBag a
 singleton v = LB [(v, 1)]
 
+fromList :: Eq a => [a] -> ListBag a
+fromList lst = LB(toTupleList lst)
+
+--operations
+
+-- given an element el, counts the number of elements of the list that are equals to el
 count :: (Foldable t, Eq a1, Num a2) => a1 -> t a1 -> a2
 count e = foldr (\x acc -> if x == e then acc + 1 else acc) 0 
 
+-- used to convert a list of elements to a list of tuples where the first element is the element 
+-- of the list and the second element its multiplicity inside the list
 toTupleList :: (Num b, Eq a) => [a] -> [(a, b)]
 toTupleList [] = []
 toTupleList (x:xs) = ((x, 1 + count x xs):(toTupleList (filter (/= x) xs)))
-
-fromList :: Eq a => [a] -> ListBag a
-fromList lst = LB(toTupleList lst)
 
 isEmpty :: ListBag a -> Bool
 isEmpty (LB []) = True
--- part1/ex2.hs
+++ part1/ex2.hs
@@ -12,3 +12,22 @@ instance Foldable ListBag where
 mapLB :: Eq a1 => (a2 -> a1) -> ListBag a2 -> ListBag a1
 mapLB f bag = fromList (map f (toList bag))
 
+
+{-
+    instance Functor ListBag where
+    fmap = mapLB
+
+    No instance for (Eq b) arising from a use of `mapLB'
+      Possible fix:
+        add (Eq b) to the context of
+          the type signature for:
+            fmap :: forall a b. (a -> b) -> ListBag a -> ListBag b
+    * In the expression: mapLB
+      In an equation for `fmap': fmap = mapLB
+      In the instance declaration for `Functor ListBag'
+
+
+    This error is caused by the additional constraint (a1 of type Eq) in mapLB, needed in order to use the function fromList. 
+    Instead, the fmap method, with type fmap :: (a -> b) -> f a -> f b, doesn't impose any constraint on the type a of the elements 
+    that are inside the container
+-}
